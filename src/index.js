49bbdc2c11b19d207ad2daaf1b065dd7d84e6620
moonish-nutcase <munia.sircar@hotmail.com>
2022-05-20 19:28:04 -0500

    Added the one call API into JS file

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ function showWeather(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 //
@@ -132,7 +134,8 @@ let celsiusLink = document.querySelector("#celsius-unit");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 //
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let forecastDays = ["Mon", "Tues", "Wed", "Thurs", "Fri"];
@@ -155,7 +158,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-//
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "282a65d87fe4f125dd5236607b9c31e9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 browseCity("Dhaka");
-displayForecast();
